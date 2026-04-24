342329860c907c973359ee1976a4892889a734d6
kakauzinha-cv <cantelli.kauany@escola.pr.gov.br>
2026-04-24 12:50:49 +0000

    criando os primeiros containers

--- lib/paginas/aula01.dart
+++ lib/paginas/aula01.dart
@@ -9,9 +9,33 @@ class Aula01 extends StatelessWidget {
       appBar: AppBar(
         title: Text("01 - Container"),
       ),
+      //o center centraliza o nosso conteudo
       body: Center(
-        child: Container(
-          child: Text("Container"),
+        //culumn coloca vários elementos como filhos 
+        //organiza os elementos na vertical 
+        child: Column(
+          children: [
+            //containeer com largura e altura
+            Center(
+              child: Container(
+                width: double. infinity, // Retorna o valor total disponivel
+                height: 100,
+                color: Colors.purple, 
+                child: Center(child: Text("primeiro container")),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              margin: EdgeInsets.all(16)
+              color:Colors.amber,
+              child: const Center(
+                child: Text(
+                  "Segundo container",
+                  style: TextStyle(color: colors.whites),
+                )
+              )
+              ,)
+          ],
         ),
       ),
     );
